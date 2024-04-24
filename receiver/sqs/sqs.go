34782789f19f13412c40cf7f9abd3e9c6abe13cf
Michael Fox <mfox@amplify.security>
2024-04-24 16:42:13 -0500

    ENG-553 continued work on sqs

--- receiver/sqs/sqs.go
+++ receiver/sqs/sqs.go
@@ -1,14 +1,32 @@
 package sqs
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 
 	"github.com/amplify-security/carrier/transmitter"
+	"github.com/amplify-security/probe/pool"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+)
+
+const (
+	// ApproxomiteReceiveCountSQSAttribute SQSAttributes key for the approximate number of times the message has been received.
+	ApproxomiteReceiveCountSQSAttribute = "ApproximateReceiveCount"
+	// ApproxomiteFirstReceiveTimestampSQSAttribute SQSAttributes key for the time the message was first received.
+	ApproxomiteFirstReceiveTimestampSQSAttribute = "ApproximateFirstReceiveTimestamp"
+	// RecieveCountTransmitAttribute TransmitAttributes key for number of times the message has been received.
+	ReceiveCountTransmitAttribute = "Receive-Count"
+	// FirstReceiveTimeTransmitAttribute TransmitAttributes key for the time the message was first received.
+	FirstReceiveTimeTransmitAttribute = "First-Receive-Time"
+)
+
+var (
+	errNoMessageBody = errors.New("message body is nil")
 )
 
 type (
@@ -22,6 +40,8 @@ type (
 	// MessageWriter interface defines the write messages API for SQS. This interface
 	// allows for mocking the SQS client in tests.
 	MessageWriter interface {
+		DeleteMessageBatch(context.Context, *sqs.DeleteMessageBatchInput, ...func(*sqs.Options)) (*sqs.DeleteMessageBatchOutput, error)
+		ChangeMessageVisibilityBatch(context.Context, *sqs.ChangeMessageVisibilityBatchInput, ...func(*sqs.Options)) (*sqs.ChangeMessageVisibilityBatchOutput, error)
 	}
 
 	// MessageReadWriter interface defines the read and write messages API for SQS. This interface
@@ -57,8 +77,92 @@ type (
 		batchSize         int32
 		transmitter       Transmitter
 		ctx               context.Context
+		cancel            context.CancelFunc
+		pool              *pool.Pool
+		messages          chan *message
+		results           chan *transmitResult
+	}
+
+	// message encapsulates the SQS message and attributes.
+	message struct {
+		MessageID     *string
+		ReceiptHandle *string
+		Body          *string
+		Attributes    map[string]string
+	}
+
+	// transmitResult encapsulates the result of a transmit operation.
+	transmitResult struct {
+		MessageID     *string
+		ReceiptHandle *string
+		err           error
+	}
+
+	// handlerConfig encapsulates all configuration settings for the Handler.
+	handlerConfig struct {
+		Transmitter Transmitter
+		Ctx         context.Context
+		Work        chan *message
+		Results     chan *transmitResult
+	}
+
+	// handler handles the processing of messages received from the SQS queue.
+	handler struct {
+		transmitter Transmitter
+		ctx         context.Context
+		work        chan *message
+		results     chan *transmitResult
 	}
 )
+
+// newHandler initializes and returns a new MessageHandler.
+func newHandler(c *handlerConfig) *handler {
+	return &handler{
+		transmitter: c.Transmitter,
+		ctx:         c.Ctx,
+		work:        c.Work,
+		results:     c.Results,
+	}
+}
+
+// generateAttributes generates the TransmitAttributes based on the SQS message attributes.
+func (h *handler) generateAttributes(m *message) transmitter.TransmitAttributes {
+	attributes := make(transmitter.TransmitAttributes)
+	for k, v := range m.Attributes {
+		switch k {
+		case ApproxomiteReceiveCountSQSAttribute:
+			attributes[ReceiveCountTransmitAttribute] = v
+		case ApproxomiteFirstReceiveTimestampSQSAttribute:
+			attributes[FirstReceiveTimeTransmitAttribute] = v
+		}
+	}
+	return attributes
+
+}
+
+// handleMessage handles the message by transmitting it using the configured transmitter.
+func (h *handler) handleMessage(m *message) error {
+	if m.Body == nil {
+		return fmt.Errorf("sqs.handler.handleMessage: %w", errNoMessageBody)
+	}
+	buf := bytes.NewBuffer([]byte(*m.Body))
+	return h.transmitter.Tx(buf, h.generateAttributes(m))
+}
+
+// handleMessages transmits messages received on the work channel and submits results to the results channel.
+func (h *handler) handleMessages() {
+	for {
+		select {
+		case <-h.ctx.Done():
+			return
+		case m := <-h.work:
+			h.results <- &transmitResult{
+				MessageID: m.MessageID,
+				err:       h.handleMessage(m),
+			}
+		}
+	}
+}
 
 // NewReceiver initializes and returns a new LongPoller.
 func NewReceiver(c *ReceiverConfig) *Receiver {
@@ -70,6 +174,24 @@ func NewReceiver(c *ReceiverConfig) *Receiver {
 		log.Error("failed to get queue URL", "queue_name", c.SQSQueueName, "error", err)
 		panic(err)
 	}
+	messages := make(chan *message, c.BatchSize)
+	results := make(chan *transmitResult, c.BatchSize)
+	ctx, cancel := context.WithCancel(context.Background())
+	p := pool.NewPool(&pool.PoolConfig{
+		Size:       c.BatchSize,
+		BufferSize: c.BatchSize,
+		Ctx:        ctx,
+	})
+	for range c.BatchSize {
+		// start message handlers
+		h := newHandler(&handlerConfig{
+			Transmitter: c.Transmitter,
+			Ctx:         ctx,
+			Work:        messages,
+			Results:     results,
+		})
+		p.Run(h.handleMessages)
+	}
 	return &Receiver{
 		log:               log,
 		client:            c.SQSClient,
@@ -78,16 +200,80 @@ func NewReceiver(c *ReceiverConfig) *Receiver {
 		batchSize:         int32(c.BatchSize),
 		transmitter:       c.Transmitter,
 		ctx:               c.Ctx,
+		cancel:            cancel,
+		pool:              p,
 	}
 }
 
-// Rx starts the LongPoller event loop to receive messages.
+// processMessages processes any messages in the SQS receive message response.
+func (p *Receiver) processMessages(res *sqs.ReceiveMessageOutput) {
+	if len(res.Messages) == 0 {
+		// empty receive, nothing to do here
+		return
+	}
+	for _, msg := range res.Messages {
+		p.messages <- &message{
+			MessageID:     msg.MessageId,
+			ReceiptHandle: msg.ReceiptHandle,
+			Body:          msg.Body,
+			Attributes:    msg.Attributes,
+		}
+	}
+	deleteEntries := []types.DeleteMessageBatchRequestEntry{}
+	retryEntries := []types.ChangeMessageVisibilityBatchRequestEntry{}
+	for range len(res.Messages) {
+		// this channel read will block until all messages from the previous batch are handled
+		r := <-p.results
+		if r.err != nil {
+			var err *transmitter.TransmitRetryableError
+			if errors.As(r.err, &err) {
+				// update visibility timeout on retryable errors
+				retryEntries = append(retryEntries, types.ChangeMessageVisibilityBatchRequestEntry{
+					Id:                r.MessageID,
+					ReceiptHandle:     r.ReceiptHandle,
+					VisibilityTimeout: int32(err.RetryAfter.Seconds()),
+				})
+			} else {
+				p.log.Error("failed to transmit message", "error", r.err)
+			}
+		} else {
+			deleteEntries = append(deleteEntries, types.DeleteMessageBatchRequestEntry{
+				Id:            r.MessageID,
+				ReceiptHandle: r.ReceiptHandle,
+			})
+		}
+	}
+	if len(deleteEntries) > 0 {
+		// delete messages that were successfully transmitted
+		_, err := p.client.DeleteMessageBatch(p.ctx, &sqs.DeleteMessageBatchInput{
+			QueueUrl: &p.queueURL,
+			Entries:  deleteEntries,
+		})
+		if err != nil {
+			p.log.Error("failed to delete messages", "error", err)
+		}
+	}
+	if len(retryEntries) > 0 {
+		// update visibility timeout on messages that received retryable errors
+		_, err := p.client.ChangeMessageVisibilityBatch(p.ctx, &sqs.ChangeMessageVisibilityBatchInput{
+			QueueUrl: &p.queueURL,
+			Entries:  retryEntries,
+		})
+		if err != nil {
+			p.log.Error("failed to update message visibility", "error", err)
+		}
+	}
+}
+
+// Rx starts the Receiver event loop to receive messages.
 func (p *Receiver) Rx() {
 	p.log.Info("starting event loop")
 	for {
 		select {
 		case <-p.ctx.Done():
 			p.log.Info("stopping event loop")
+			p.cancel()
+			p.pool.Stop(true)
 			return
 		default:
 			// poll for new messages on the queue
@@ -95,10 +281,11 @@ func (p *Receiver) Rx() {
 				QueueUrl:            &p.queueURL,
 				MaxNumberOfMessages: p.batchSize,
 				VisibilityTimeout:   p.visibilityTimeout,
+				WaitTimeSeconds:     20,
 				// this is a workaround until aws-sdk-go-v2 fixes issue #2124 https://github.com/aws/aws-sdk-go-v2/issues/2124
 				AttributeNames: []types.QueueAttributeName{
-					types.QueueAttributeName("ApproximateReceiveCount"),
-					types.QueueAttributeName("ApproximateFirstReceiveTimestamp"),
+					types.QueueAttributeName(ApproxomiteReceiveCountSQSAttribute),
+					types.QueueAttributeName(ApproxomiteFirstReceiveTimestampSQSAttribute),
 				},
 			})
 			if err != nil {
@@ -107,9 +294,8 @@ func (p *Receiver) Rx() {
 				}
 				continue
 			}
-			for _, msg := range res.Messages {
-				p.log.Info("received message", "message_id", *msg.MessageId, "body", *msg.Body, "attributes", msg.Attributes)
-			}
+			p.processMessages(res)
+			// continue
 		}
 	}
 }
--- receiver/sqs/sqs_test.go
+++ receiver/sqs/sqs_test.go
@@ -3,32 +3,212 @@ package sqs
 import (
 	"context"
 	"errors"
+	"io"
 	"log/slog"
 	"os"
 	"testing"
 
+	"github.com/amplify-security/carrier/transmitter"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 type (
-	// MockMessageReadWriter is a mock implementation of the MessageReadWriter interface.
-	MockMessageReadWriter struct {
+	// mockMessageReadWriter is a mock implementation of the MessageReadWriter interface.
+	mockMessageReadWriter struct {
+		mock.Mock
+	}
+
+	// mockTransmitter is a mock implementation of the Transmitter interface.
+	mockTransmitter struct {
 		mock.Mock
 	}
 )
 
 // GetQueueUrl implementation of the MessageReadWriter interface for the MockMessageReadWriter.
-func (m *MockMessageReadWriter) GetQueueUrl(ctx context.Context, input *sqs.GetQueueUrlInput, opts ...func(*sqs.Options)) (*sqs.GetQueueUrlOutput, error) {
+func (m *mockMessageReadWriter) GetQueueUrl(ctx context.Context, input *sqs.GetQueueUrlInput, opts ...func(*sqs.Options)) (*sqs.GetQueueUrlOutput, error) {
 	args := m.Called(ctx, input, opts)
 	return args.Get(0).(*sqs.GetQueueUrlOutput), args.Error(1)
 }
 
 // ReceiveMessage implementation of the MessageReadWriter interface for the MockMessageReadWriter.
-func (m *MockMessageReadWriter) ReceiveMessage(ctx context.Context, input *sqs.ReceiveMessageInput, opts ...func(*sqs.Options)) (*sqs.ReceiveMessageOutput, error) {
+func (m *mockMessageReadWriter) ReceiveMessage(ctx context.Context, input *sqs.ReceiveMessageInput, opts ...func(*sqs.Options)) (*sqs.ReceiveMessageOutput, error) {
 	args := m.Called(ctx, input, opts)
 	return args.Get(0).(*sqs.ReceiveMessageOutput), args.Error(1)
+}
+
+// DeleteMessageBatch implementation of the MessageReadWriter interface for the MockMessageReadWriter.
+func (m *mockMessageReadWriter) DeleteMessageBatch(ctx context.Context, input *sqs.DeleteMessageBatchInput, opts ...func(*sqs.Options)) (*sqs.DeleteMessageBatchOutput, error) {
+	args := m.Called(ctx, input, opts)
+	return args.Get(0).(*sqs.DeleteMessageBatchOutput), args.Error(1)
+}
+
+// ChangeMessageVisibilityBatch implementation of the MessageReadWriter interface for the MockMessageReadWriter.
+func (m *mockMessageReadWriter) ChangeMessageVisibilityBatch(ctx context.Context, input *sqs.ChangeMessageVisibilityBatchInput, opts ...func(*sqs.Options)) (*sqs.ChangeMessageVisibilityBatchOutput, error) {
+	args := m.Called(ctx, input, opts)
+	return args.Get(0).(*sqs.ChangeMessageVisibilityBatchOutput), args.Error(1)
+}
+
+// Tx implementation of the Transmitter interface for the MockTransmitter.
+func (m *mockTransmitter) Tx(r io.Reader, a transmitter.TransmitAttributes) error {
+	args := m.Called(r, a)
+	return args.Error(0)
+}
+
+func TestNewHandler(t *testing.T) {
+	h := newHandler(&handlerConfig{
+		Transmitter: &mockTransmitter{},
+		Ctx:         context.Background(),
+		Work:        make(chan *message, 1),
+		Results:     make(chan *transmitResult, 1),
+	})
+	assert.NotNil(t, h, "newHandler -> handler != nil")
+}
+
+func TestHandler_handleMessage(t *testing.T) {
+	testErr := errors.New("mockTransmitter failed to transmit message")
+	cases := []struct {
+		m        *message
+		txErr    error
+		expected error
+	}{
+		{
+			m: &message{
+				Body: aws.String("foo"),
+			},
+		},
+		{
+			m:        &message{},
+			expected: errNoMessageBody,
+		},
+		{
+			m: &message{
+				Body: aws.String("foo"),
+			},
+			expected: testErr,
+			txErr:    testErr,
+		},
+	}
+	for _, c := range cases {
+		tx := &mockTransmitter{}
+		if c.txErr == nil {
+			tx.On("Tx", mock.Anything, mock.Anything).Return(nil)
+		} else {
+			tx.On("Tx", mock.Anything, mock.Anything).Return(c.txErr)
+		}
+		h := newHandler(&handlerConfig{
+			Transmitter: tx,
+			Ctx:         context.Background(),
+		})
+		err := h.handleMessage(c.m)
+		if c.expected != nil {
+			if !errors.Is(c.expected, errNoMessageBody) {
+				tx.AssertCalled(t, "Tx", mock.Anything, mock.Anything)
+			}
+			assert.ErrorIs(t, err, c.expected, "handler.handleMessage -> error")
+		} else {
+			assert.Nil(t, err, "handler.handleMessage -> nil")
+			tx.AssertCalled(t, "Tx", mock.Anything, mock.Anything)
+		}
+	}
+}
+
+func TestHandler_generateAttributes(t *testing.T) {
+	cases := []struct {
+		m        *message
+		expected transmitter.TransmitAttributes
+	}{
+		{
+			m: &message{
+				Attributes: map[string]string{
+					ApproxomiteReceiveCountSQSAttribute:          "1",
+					ApproxomiteFirstReceiveTimestampSQSAttribute: "2021-09-01T00:00:00Z",
+				},
+			},
+			expected: transmitter.TransmitAttributes{
+				ReceiveCountTransmitAttribute:     "1",
+				FirstReceiveTimeTransmitAttribute: "2021-09-01T00:00:00Z",
+			},
+		},
+		{
+			m: &message{
+				Attributes: map[string]string{
+					"UnsupportedKey": "1",
+				},
+			},
+			expected: transmitter.TransmitAttributes{},
+		},
+		{
+			m:        &message{},
+			expected: transmitter.TransmitAttributes{},
+		},
+	}
+	for _, c := range cases {
+		h := newHandler(&handlerConfig{
+			Ctx: context.Background(),
+		})
+		attrs := h.generateAttributes(c.m)
+		assert.Equal(t, c.expected, attrs, "handler.generateAttributes")
+	}
+}
+
+func TestHandler_handleMessages(t *testing.T) {
+	testErr := errors.New("mockTransmitter failed to transmit message")
+	cases := []struct {
+		m        *message
+		txErr    error
+		expected error
+	}{
+		{
+			m: &message{
+				MessageID: aws.String("foo"),
+				Body:      aws.String("bar"),
+			},
+		},
+		{
+			m: &message{
+				MessageID: aws.String("foo"),
+			},
+			expected: errNoMessageBody,
+		},
+		{
+			m: &message{
+				MessageID: aws.String("foo"),
+				Body:      aws.String("bar"),
+			},
+			expected: testErr,
+			txErr:    testErr,
+		},
+	}
+	for _, c := range cases {
+		tx := &mockTransmitter{}
+		if c.txErr == nil {
+			tx.On("Tx", mock.Anything, mock.Anything).Return(nil)
+		} else {
+			tx.On("Tx", mock.Anything, mock.Anything).Return(c.txErr)
+		}
+		messages := make(chan *message, 1)
+		results := make(chan *transmitResult, 1)
+		ctx, cancel := context.WithCancel(context.Background())
+		h := newHandler(&handlerConfig{
+			Transmitter: tx,
+			Ctx:         ctx,
+			Work:        messages,
+			Results:     results,
+		})
+		go h.handleMessages()
+		messages <- c.m
+		r := <-results
+		cancel()
+		assert.Equal(t, c.m.MessageID, r.MessageID, "handler.handleMessages -> message ID")
+		if c.expected != nil {
+			assert.ErrorIs(t, r.err, c.expected, "handler.handleMessages -> error")
+		} else {
+			assert.Nil(t, r.err, "handler.handleMessages -> nil")
+		}
+	}
 }
 
 func TestNewReceiver(t *testing.T) {
@@ -45,7 +225,7 @@ func TestNewReceiver(t *testing.T) {
 	}
 	for _, c := range cases {
 		var rx *Receiver
-		mockClient := &MockMessageReadWriter{}
+		mockClient := &mockMessageReadWriter{}
 		mockClient.On("GetQueueUrl", mock.Anything, mock.Anything, mock.Anything).Return(&sqs.GetQueueUrlOutput{
 			QueueUrl: &c.queueURL,
 		}, c.getQueueURLErr)
